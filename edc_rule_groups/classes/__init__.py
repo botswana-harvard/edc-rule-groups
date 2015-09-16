edcac3b5d85244d2da734ae7cb236991c5af723a
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-09-16 11:12:10 +0200

    test file base rule

--- edc_rule_groups/classes/__init__.py
+++ edc_rule_groups/classes/__init__.py
@@ -1,4 +1,5 @@
 from .base_rule import BaseRule
+from .base_rule_provider import BaseRuleProvider
 from .controller import Controller, site_rule_groups
 from .dashboard_rule import DashboardRule
 from .logic import Logic
